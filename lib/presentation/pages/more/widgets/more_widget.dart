5703b6b6e9f628e78248e23ded837481a1ba64e6
MhmdMhros <mhrwsmhmd8882@gmail.com>
2025-03-12 07:58:39 +0200

    privacy and security screen added.

--- lib/presentation/pages/more/widgets/more_widget.dart
+++ lib/presentation/pages/more/widgets/more_widget.dart
@@ -16,45 +16,48 @@ class MoreWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          width: 30.w,
-          height: 30.h,
-          decoration: BoxDecoration(
-            color: AppColors.iconsBack,
-            borderRadius: BorderRadius.circular(4.r),
+    return InkWell(
+      onTap: () => onTap(),
+      child: Row(
+        children: [
+          Container(
+            width: 30.w,
+            height: 30.h,
+            decoration: BoxDecoration(
+              color: AppColors.iconsBack,
+              borderRadius: BorderRadius.circular(4.r),
+            ),
+            child: Icon(
+              icon,
+              size: 20.sp,
+              color: AppColors.primary,
+            ),
           ),
-          child: Icon(
-            icon,
-            size: 20.sp,
-            color: AppColors.primary,
+          SizedBox(width: 16.w),
+          Text(
+            title,
+            style: TextStyle(
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w500,
+              fontFamily: "Montserrat", // Applied font family
+              color: AppColors.text1,
+            ),
           ),
-        ),
-        SizedBox(width: 16.w),
-        Text(
-          title,
-          style: TextStyle(
-            fontSize: 14.sp,
-            fontWeight: FontWeight.w500,
-            fontFamily: "Montserrat", // Applied font family
-            color: AppColors.text1,
-          ),
-        ),
-        Expanded(
-          child: Align(
-            alignment: Alignment.centerRight,
-            child: IconButton(
-              onPressed: () => onTap(),
-              icon: Icon(
-                Icons.arrow_forward_ios,
-                size: 20.sp,
-                color: AppColors.primary,
+          Expanded(
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: IconButton(
+                onPressed: () => onTap(),
+                icon: Icon(
+                  Icons.arrow_forward_ios,
+                  size: 20.sp,
+                  color: AppColors.primary,
+                ),
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
